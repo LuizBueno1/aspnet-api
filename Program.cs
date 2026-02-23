29f66b285cac3855f38873aaabc6eaf4506e2944
LuizBueno1 <luizbueno.dev@gmail.com>
2026-02-23 15:56:44 -0300

    Create and connect database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 // Create the WebApplicationBuilder to configure services and the HTTP pipeline.
 var builder = WebApplication.CreateBuilder(args);
 
+//Connecting database
+var rawConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString =
+    rawConnection
+        .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+        .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+        .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+        .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
+
+builder.Services.AddSingleton(new PersonRepository(connectionString));
+
 // Register MVC controller services.
 builder.Services.AddControllers();
 
